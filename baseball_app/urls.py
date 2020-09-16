e8d1bdb4862975a7b74a256c59e2653135666b00
ee090186 <ee090186@gmail.com>
2020-09-16 22:02:37 +0900

    Add: ログイン画面作成のためlogin.html追加 Fix: ログイン画面作成のためviews,urls修正

--- baseball_app/urls.py
+++ baseball_app/urls.py
@@ -2,11 +2,13 @@ from os import name
 from django.contrib import admin
 from django.db.models.query_utils import PathInfo
 from django.urls import path
-from .views import register_user
+from .views import register_user, loginview
+
 
 
 urlpatterns = [
         path('admin/', admin.site.urls, name='admin'),
         path('register_user/', register_user, name='register_user'),
+        path('login/', loginview, name='login')
 
 ]
--- baseball_app/views.py
+++ baseball_app/views.py
@@ -10,6 +10,21 @@ from django.shortcuts import render, redirect
 from .forms import ProfileForm, UserCreateForm
 
 
+
+def loginview(request):
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        user = authenticate(request, username=username_data, password=password_data)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return redirect('login')
+
+    return render(request, 'login.html')
+
+
 def register_user(request):
     # or NoneでGet時はNoneとなり、引数なしのフォームを作る。
     user_form = UserCreateForm(request.POST or None)
