3885d8ab5a3bd0183dd068e6d7fd047441297579
ee090186 <ee090186@gmail.com>
2020-09-17 23:26:07 +0900

    Add: ユーザー情報編集画面追加のため、html追加、view追加
    * ナビバーでupdate.htmlへリンクされるところまでは確認済み

--- baseball_app/urls.py
+++ baseball_app/urls.py
@@ -2,7 +2,7 @@ from os import name
 from django.contrib import admin
 from django.db.models.query_utils import PathInfo
 from django.urls import path
-from .views import registerview, loginview, homeview, logoutview
+from .views import registerview, loginview, homeview, logoutview, updateview
 
 
 
@@ -12,5 +12,6 @@ urlpatterns = [
         path('login/', loginview, name='login'),
         path('home/', homeview, name='home'),
         path('logout/', logoutview, name='logout'),
+        path('update/<int:pk>', updateview, name='update'),
 
 ]
--- baseball_app/views.py
+++ baseball_app/views.py
@@ -4,11 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
 from django.template.context_processors import request
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProfileForm, UserCreateForm
-
+from .models import Profile
 
 
 def registerview(request):
@@ -53,6 +53,7 @@ def logoutview(request):
     logout(request)
     return redirect('login')
 
+
 @login_required()
 def homeview(request):
     params = {
@@ -61,3 +62,19 @@ def homeview(request):
 
     return render(request, 'home.html', params)
     
+@login_required()
+def updateview(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    profile = Profile.objects.get(id=pk)
+    user_form = UserCreateForm(request.POST or None, instance=user)
+    profile_form = ProfileForm(request.POST or None, instance=profile)
+    if request.method == "POST" and user_form.is_valid() and profile_form.is_valid():
+        user_form.save()
+        profile_form.save()
+        return redirect("home")
+
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form,
+    }
+    return render(request, 'update.html', context)
