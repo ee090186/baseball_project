b0c92cca765b5247df191cb9851863df09e5998e
ee090186 <ee090186@gmail.com>
2020-09-24 22:46:33 +0900

    Add: PittingModel,BattingModel,SituationModelの追加
    * マイグレーション未実施

--- baseball_app/models.py
+++ baseball_app/models.py
@@ -1,8 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
-
-
+from django.db.models.fields import BooleanField, IntegerField
 
 
 class Profile(models.Model):
@@ -35,7 +33,7 @@ class Profile(models.Model):
         ('left_handed_throwing', '左投げ'),
         ('switch_picher', '両投げ'),
     )
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField('氏名', max_length=100)
     gender = models.CharField('性別', max_length=5, choices=GENDER_CHOICES)
     birthday = models.DateField('生年月日', blank=True)
@@ -47,7 +45,131 @@ class Profile(models.Model):
     batting_handedness = models.CharField('打ち方', max_length=20, choices=BATTING_HANDEDNESS_CHOICES)
     throwing_handedness = models.CharField('投げ方', max_length=21, choices=THROWING_HANDEDNESS_CHOICES)
     team = models.CharField('チーム名', max_length=100, blank=True, null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.name
+
+
+
+class Situation(models.Model):
+    BASE_CHOICES = (
+        ('0', 'ランナーなし'),
+        ('1', '1塁'),
+        ('2', '2塁'),
+        ('3', '3塁'),
+        ('1-2', '1塁2塁'),
+        ('1-3', '1塁3塁'),
+        ('2-3', '2塁3塁'),
+        ('1-2-3', '満塁'),
+    )
+
+    OUTS_CHOICES = (
+        ('0', '0アウト'),
+        ('1', '1アウト'),
+        ('2', '2アウト'),
+    )
+
+    BAll_COUNT_CHOICES = (
+        ('0', '0ボール'),
+        ('1', '1ボール'),
+        ('2', '2ボール'),
+        ('3', '3ボール'),
+    )
+
+    STRIKE_COUNT_CHOICES = (
+        ('0', '0ストライク'),
+        ('1', '1ストライク'),
+        ('2', '2ストライク'),
+    )
+
+    INNIG_CHOICES = (
+        ('1', '1回'),
+        ('2', '2回'),
+        ('3', '3回'),
+        ('4', '4回'),
+        ('5', '5回'),
+        ('6', '6回'),
+        ('7', '7回'),
+        ('8', '8回'),
+        ('9', '9回'),
+        ('10', '10回'),
+        ('11', '11回'),
+        ('12', '12回以上'),
+    )
+    base = models.CharField('出塁状況', max_length=20, choices=BASE_CHOICES)
+    outs = models.CharField('アウトカウント', max_length=20, choices=OUTS_CHOICES)
+    ball_count = models.CharField('ボール', max_length=20, choices=BAll_COUNT_CHOICES)
+    strike_count = models.CharField('ストライク', max_length=20, choices=STRIKE_COUNT_CHOICES)
+    inning = models.CharField('回', max_length=20, choices=INNIG_CHOICES)
+    def __str__(self):
+        return self.inning + '(' + self.outs + ')'
+
+
+
+class Pitting(models.Model):
+    SIDE_COURSE_CHOICES = (
+        ('inside', '内角'),
+        ('near_inside', '内角寄り'),
+        ('middle', '真ん中'),
+        ('near_outside', '外角寄り'),
+        ('outside', '外角'),
+    )
+
+    HEIGHT_COURSE_CHOICES = (
+        ('low', '低め'),
+        ('near_low', '低め寄り'),
+        ('middle', '真ん中'),
+        ('near_high', '高め寄り'),
+        ('high', '高め'),
+    )
+
+    SPEED_CHOICES = (
+        ('100', '100km以下'),
+        ('110', '110km台'),
+        ('120', '120km台'),
+        ('130', '130km台'),
+        ('140', '140km台'),
+        ('150', '150km台'),
+        ('160', '160km以上'),
+    )
+
+    TYPE_OF_PITCH_CHOICES = (
+        ('4sfb', 'ストレート'),
+        ('cb', 'カーブ'),
+        ('sl', 'スライダー'),
+        ('frk', 'フォーク'),
+        ('cut', 'カットボール'),
+        ('scr', 'スクリュー'),
+        ('ch', 'チェンジアップ'),
+        ('kn', 'ナックル'),
+        ('2sfb', 'ツーシーム'),
+        ('rfb', 'シュート'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    situation = models.OneToOneField(Situation, on_delete=models.CASCADE)
+    side_corse = models.CharField('左右のコース', max_length=20, choices=SIDE_COURSE_CHOICES)
+    height_corse = models.CharField('上下のコース', max_length=20, choices=HEIGHT_COURSE_CHOICES)
+    speed = models.CharField('球速', max_length=20, choices=SPEED_CHOICES)
+    type_of_pitch = models.CharField('球種', max_length=20, choices=TYPE_OF_PITCH_CHOICES)
+    pichout_or_waste = models.BooleanField('ピッチアウト,捨て球', default=False)
+    number_of_pitches = models.IntegerField('球数')
+    def __str__(self):
+        return str(self.user) + '(' + self.number_of_pitches + ')'
+
+
+
+class Batting(models.Model):
+    BATTING_CHOICES = (
+        ('swing', 'スイング'),
+        ('looking', '見送り'),
+        ('bunt', '送りバント'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pitting = models.OneToOneField(Pitting, on_delete=models.CASCADE)
+    batting = models.CharField('打者の行動', max_length=7, choices=BATTING_CHOICES)
+    def __str__(self):
+        return str(self.user) + self.batting
+
+
+
+class Results(models.Model):
+    
